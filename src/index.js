9cde2d08bd78956f2521fd8b046b5a211019f6a4
Guy Lepage <guylepage3@gmail.com>
2018-11-22 11:41:25 -0500

    created initial component
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class CopyrightUpdate extends React.Component {
+  render() {
+    return (
+      <div>This is so CopyrightUpdate!</div>
+    );
+  }
+}
+export default CopyrightUpdate;
