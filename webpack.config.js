57e472e2e638ae7dd6381f26ef6a2fe990164d74
Guy Lepage <guylepage3@gmail.com>
2018-11-23 16:35:50 -0500

    update to subtract node_modules
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,11 @@ module.exports = {
       }
     ]
   },
-  externals: {
-    'react': 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  },
-  target: 'node', // in order to ignore built-in modules like path, fs, etc. 
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  target: 'node',
+  externals: [
+    {
+      react: 'commonjs react',
+    },
+    subtract: [nodeExternals()]
+  ]
 };
