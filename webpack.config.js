f70fb1a80140738ca19f959c60eab407f96d31e6
Guy Lepage <guylepage3@gmail.com>
2018-11-23 12:01:12 -0500

    exclude node_modules export
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ module.exports = {
       }
     ]
   },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc. 
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   externals: {
     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   }
