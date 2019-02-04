baa024b99586614d05004448626fd5e13310e3c1
Guy Lepage <guylepage3@gmail.com>
2019-02-04 15:07:45 -0500

    added style support
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class CopyrightUpdate extends React.Component {
+class CopyrightUpdate extends Component {
   render() {
     return React.createElement(
       "div",
       {className: 'copyright-update'},
+      this.props.style,
       null,
       '\u00A9 ',
       new Date().getFullYear()
