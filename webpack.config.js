c18243d4942e58985f27d9d3b91c2a4b075e9eb8
Modestos Kaffes <modkaffes@gmail.com>
2019-08-30 14:55:54 +0300

    Update webpack options

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-react-jsx']
           }
         }
       }
