5a1e411be492eebfbdb0d0e02ecf0d4aae86495c
Guy Lepage <guylepage3@gmail.com>
2023-05-01 16:51:04 -0700

    added inline
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CopyrightUpdate = () => {
   return (
-    <div className="copyright-update">
+    <div className="copyright-update" style={{ display: 'inline-block' }}>
       &copy; {new Date().getFullYear()}
     </div>
   );
